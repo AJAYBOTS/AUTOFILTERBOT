d58daa18b7f5b9685cb5330138f03b7c201ebe6e
AJAYBOTS <channelallin0@gmail.com>
2024-09-02 23:21:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TOICTONY/Ops.git /Jack-Reacher-Bot
+  git clone https://github.com/AJAYBOTS/AUTOFILTERBOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Jack-Reacher-Bot
+  git clone $UPSTREAM_REPO /AUTOFILTERBOT
 fi
 cd /Jack-Reacher-Bot
 pip3 install -U -r requirements.txt
-echo "Starting Jack-Reacher-Bot 😎...."
+echo "Starting  AUTOFILTERBOT😎...."
 python3 bot.py    
